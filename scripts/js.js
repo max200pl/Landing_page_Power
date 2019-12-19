ca744b41f52f4b5420d13b30ad2be7b57b4a9805
max200pl <max2000pl@gmail.com>
2019-12-19 09:41:10 +0200

    We are working on a slider

---
+++ scripts/js.js
@@ -0,0 +1,49 @@
+
+
+$('.sl').slick({
+
+    slidesToShow: 5,
+    centerMode: true,
+    centerPadding: '0px',
+
+    arrows: false,
+   /* responsive: [
+        {
+            breakpoint: 1100,
+            settings: {
+                slidesToShow: 5
+            }
+    },
+        {
+            breakpoint: 960,
+            settings: {
+                slidesToShow: 3
+
+            }
+    },
+        {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 2
+
+            }
+    },
+        {
+            breakpoint: 640,
+            settings: {
+                slidesToShow: 1
+
+            }
+    },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                dots: false,
+                centerMode: false
+
+            }
+    }
+  ]*/
+
+});
