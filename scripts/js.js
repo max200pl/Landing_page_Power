6656b3600b0b6284b373ca58c3679e0e8c35b4af
max200pl <max2000pl@gmail.com>
2019-12-22 13:43:48 +0200

    change slider2; slider2 comlete; change template

--- scripts/js.js
+++ scripts/js.js
@@ -1,3 +1,5 @@
+ /*-------------slider 1--------------*/
+
 const worksSlider = $('[data-slider="sl"]');
 
 worksSlider.slick({
@@ -69,4 +71,76 @@ $(".slickNext").on("click", function(event){
 
 /*Bg slider*/
 
+/*--------------/slider 1--------------*/
 
+ /*-------------slider 2 --------------*/
+
+const worksSliderTwo = $('[data-slider="sl2"]');
+
+worksSliderTwo.slick({
+
+    slidesToShow: 3,
+    arrows: false
+    /* responsive: [
+        {
+            breakpoint: 1100,
+            settings: {
+                slidesToShow: 5
+            }
+    },
+        {
+            breakpoint: 960,
+            settings: {
+                slidesToShow: 3
+
+            }
+    },
+        {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 2
+
+            }
+    },
+        {
+            breakpoint: 640,
+            settings: {
+                slidesToShow: 1
+
+            }
+    },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                dots: false,
+                centerMode: false
+
+            }
+    }
+  ]*/
+
+});
+
+
+/*Button slider*/
+
+$(".slickPrev").on("click", function (event) {
+    event.preventDefault();
+
+    let currentSlider = $(this).parents('.reviews').find('[data-slider="sl2"]');
+
+    currentSlider.slick("slickPrev");
+});
+
+$(".slickNext").on("click", function(event){
+    event.preventDefault();
+
+    let currentSlider = $(this).parents('.reviews').find('[data-slider="sl2"]');
+
+    currentSlider.slick("slickNext");
+});
+
+/*Bg slider*/
+
+/*--------------/slider--------------*/
